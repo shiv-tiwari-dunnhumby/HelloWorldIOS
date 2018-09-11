8d1cb22888050ef6271927c7d977269156be05e4
shivendratiwari81 <shivendratiwari81@gmail.com>
2018-09-11 15:22:28 +0200

    Add files via upload

---
+++ HelloWorldIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorldIOS
+//
+//  Created by Shivendra Tiwari on 11.09.18.
+//  Copyright © 2018 Shivendra Tiwari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
